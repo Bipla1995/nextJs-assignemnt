24839fda6fc84b0934a333fd1f8c9d687ddb3a6c
EMBITEL\emb-biplkum <biplab.kumarjena@embitel.com>
2023-09-28 13:20:24 +0530

    initial commit

---
+++ app/users/server-side.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+import createApolloClient from "../apollo-client";
+export async function getServerSideProps() {
+  const client = createApolloClient();
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
+}
